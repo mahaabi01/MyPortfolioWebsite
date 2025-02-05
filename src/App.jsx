ec45e15e157c58ee1d1bc1c50703e0abd08a4439
mahaabi01 <mahaabi01@gmail.com>
2025-02-05 11:22:50 +0545

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
   <>
   <Header />
+  <main>
+    <Hero />
+  </main>
   </>
   )
 }
