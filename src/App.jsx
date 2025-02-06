5365e5c4c3d124a0f54b8085c42c5b89e4f30df2
mahaabi01 <mahaabi01@gmail.com>
2025-02-06 08:55:57 +0545

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
+import { ReactLenis } from "lenis/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+// Register gsap plugins
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Skill from "./components/Skill"
-import Work from "./components/Work"
-import Review from "./components/Review" 
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
+import Review from "./components/Review";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+    elements.forEach((element)=> {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: '-200 bottom',
+          end: 'bottom 80%',
+          scrub: true
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+      })
+    })
+  })
   return (
-  <>
-  <Header />
-  <main>
-    <Hero />
-    <About />
-    <Skill />
-    <Work />
-    <Review />
-    <Contact />
-  </main>
-  <Footer />
-  </>
-  )
-}
+    <ReactLenis root>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+        <Work />
+        <Review />
+        <Contact />
+      </main>
+      <Footer />
+    </ReactLenis>
+  );
+};
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <div className="flex items-center gap-3">
               <figure className="img-box w-9 h-9 rounded-lg">
                 <img
-                  src="./src/images/avatar-1.jpg"
+                  src="./src/images/abilash1.jpg"
                   width={40}
                   height={40}
                   alt="Abilash Maharjan Portrait"
@@ -46,8 +46,8 @@ const Hero = () => {
           <div className="hidden lg:block">
             <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
               <img
-                src="./src/images/hero-banner.png"
-                // src="./src/images/abilash1.jpg"
+                // src="./src/images/hero-banner.png"
+                src="./src/images/abilash.png"
                 width={656}
                 height={800}
                 alt="Abilash Maharjan"
